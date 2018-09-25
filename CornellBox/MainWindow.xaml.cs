6117272378dcdc0160c979f6c5529101e6f65d05
Janis <Fallyx@users.noreply.github.com>
2018-09-25 14:47:04 +0200

    Flat Cornell Box

--- CornellBox/MainWindow.xaml.cs
+++ CornellBox/MainWindow.xaml.cs
@@ -53,7 +53,7 @@ namespace CornellBox
             spheres[5] = fYellow;
             spheres[6] = gCyan;
 
-            CompositionTarget.Rendering += Render;
+            // CompositionTarget.Rendering += Render;
 
             wbmap = new WriteableBitmap(
                 imgWidth,
@@ -64,12 +64,12 @@ namespace CornellBox
                 null);
 
             cornellBoxImg.Source = wbmap;
+
+            Render();
         }
 
-        private void Render(object sener, EventArgs e)
+        private void Render()
         {
-            byte[,,] pixels = new byte[imgHeight, imgWidth, stride];
-            //byte[] pixels1d = null;
             byte[] pixels1d = new byte[imgHeight * imgWidth * 4];
             int index1d = 0;
             Ray eyeRay;
@@ -84,9 +84,6 @@ namespace CornellBox
 
                     eyeRay = CreateEyeRay(Eye, LookAt, FOV, new Vector2((float)px, (float)py));
                     hPoint = FindClosestHitPoint(spheres, eyeRay);
-                    //pixels[row, col, 0] = Convert.ToByte(hPoint.Sphere.Color.X * 255);
-                    //pixels[row, col, 1] = Convert.ToByte(hPoint.Sphere.Color.Y * 255);
-                    //pixels[row, col, 2] = Convert.ToByte(hPoint.Sphere.Color.Z * 255);
 
                     pixels1d[index1d++] = Convert.ToByte(hPoint.Sphere.Color.X * 255);
                     pixels1d[index1d++] = Convert.ToByte(hPoint.Sphere.Color.Y * 255);
@@ -95,8 +92,6 @@ namespace CornellBox
                 }
             }
 
-
-            //pixels1d = ConvertTo1d(pixels, stride, imgWidth, imgHeight);
             Int32Rect rect = new Int32Rect(0, 0, imgWidth, imgHeight);            
             wbmap.WritePixels(rect, pixels1d, stride, 0);
         }
@@ -105,15 +100,13 @@ namespace CornellBox
         {
             double alpha = FOV * Math.PI / 180.0;
             Vector3 f = Vector3.Subtract(LookAt, Eye);
-            Vector3 r = Vector3.Cross(new Vector3(0,1,0), f);
+            Vector3 r = Vector3.Cross(new Vector3(-1,0,0), f);
             Vector3 u = Vector3.Cross(r, f);
 
-            // d = normalized f + x * normalized r * tan (a/2) + y * normalizd u * tan (alpha/2)
             Vector3 d1 = Vector3.Normalize(f);
             Vector3 d2 = Vector3.Multiply(Vector3.Normalize(r), Pixel.X * (float)Math.Tan(alpha / 2));
             Vector3 d3 = Vector3.Multiply(Vector3.Normalize(u), Pixel.Y * (float)Math.Tan(alpha / 2));
 
-            //Vector3 d = Vector3.Normalize(f) + (Pixel.X * Vector3.Normalize(r) * (float) Math.Tan(FOV / 2)) + (Pixel.Y * Vector3.Normalize(u) * (float) Math.Tan(FOV / 2));
             Vector3 d = Vector3.Add(d1, Vector3.Add(d2, d3));
 
             return new Ray(Eye, Vector3.Normalize(d));
@@ -125,13 +118,13 @@ namespace CornellBox
             Sphere closestSphere = null;
 
             for (int i = 0; i < spheres.Length; i++)
-            {
-                Vector3 H = Eye + ray.Origin;
+            { 
                 double r = spheres[i].Radius;
-                Vector3 CE = ray.Origin - spheres[i].Center;
+                Vector3 CE = Vector3.Subtract(ray.Origin, spheres[i].Center);
                 float a = 1;
-                //Vector3 b = 2 * CE + ray.Direction;
-                float b = 2 * Vector3.Dot(CE, ray.Direction);
+
+                Vector3 b1 = Vector3.Multiply(CE, 2);
+                float b = Vector3.Dot(b1, Vector3.Normalize(ray.Direction)); 
                 float c = (float)(CE.Length() * CE.Length() - spheres[i].Radius * spheres[i].Radius);
 
                 if (b * b > 4 * a * c)
@@ -139,33 +132,35 @@ namespace CornellBox
                     double lambda1 = (-b + Math.Sqrt(b * b - 4 * a * c)) / 2 * a;
                     double lambda2 = (-b - Math.Sqrt(b * b - 4 * a * c)) / 2 * a;
 
-                    closestHit = Math.Min(closestHit, Math.Min(lambda1, lambda2));
-                    if(closestHit == lambda1 || closestHit == lambda2)
+                    if(lambda1 >= 0 && lambda2 >= 0)
                     {
-                        closestSphere = spheres[i];
+                        closestHit = Math.Min(closestHit, Math.Min(lambda1, lambda2));
+                        if (closestHit == lambda1 || closestHit == lambda2)
+                        {
+                            closestSphere = spheres[i];
+                        }
+                    }
+                    else if(lambda1 >= 0 && lambda2 < 0)
+                    {
+                        closestHit = Math.Min(closestHit, lambda1);
+                        if(closestHit == lambda1)
+                        {
+                            closestSphere = spheres[i];
+                        }
+                    }
+                    else if(lambda1 < 0 && lambda2 >= 0)
+                    {
+                        closestHit = Math.Min(closestHit, lambda2);
+                        if (closestHit == lambda2)
+                        {
+                            closestSphere = spheres[i];
+                        }
                     }
                 }
             }
 
             Vector3 pos = new Vector3((float)(Eye.X + closestHit * ray.Origin.X), (float)(Eye.Y + closestHit * ray.Origin.Y), (float)(Eye.Z + closestHit * ray.Origin.Z));
             return new Hitpoint(pos, closestSphere);
-        }
-
-        private byte[] ConvertTo1d(byte[,,] pixels, int stride, int imgWidth, int imgHeight)
-        {
-            byte[] pixels1d = new byte[imgHeight * imgWidth * 4];
-            int index = 0;
-
-            for (int row = 0; row < imgHeight; row++)
-            {
-                for (int col = 0; col < imgWidth; col++)
-                {
-                    for (int i = 0; i < 4; i++)
-                        pixels1d[index++] = pixels[row, col, i];
-                }
-            }
-
-            return pixels1d;
         }
     }
 }
